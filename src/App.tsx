313fdedfddeaa1ff4c27a13b9f7aa1ec88c8237b
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-08-18 08:36:26 +0100

    Set up for tracking app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import { Break } from "./components/Break";
 import { Session } from "./components/Session";
 import { Timer } from "./components/Timer";
 
 function App() {
+  const [breakTime, setBreakTime] = useState(5);
+  const [sessionTime, setSessionTime] = useState(25);
+  const [timer, setTimer] = useState(sessionTime);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+
   return (
     <div>
       <div className="flex flex-col items-center gap-6 max-w-xl mx-auto mt-16 p-12 border-2 border-gray-600 rounded-2xl">
         <div className="flex justify-between self-stretch">
-          <Break timeInMins={9} />
-          <Session timeInMins={12} />
+          <Break timeInMins={breakTime} />
+          <Session timeInMins={sessionTime} />
         </div>
-        <Timer timeInMins={25} />
+        <Timer timeInMins={timer} />
       </div>
     </div>
   );
