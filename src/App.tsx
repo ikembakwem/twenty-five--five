6066bc7b177b9a7d72ee1f22ecb495f3c2536476
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-08-18 07:26:48 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold text-blue-700 text-center">
+          VITE & TailwindCSS Starter
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
