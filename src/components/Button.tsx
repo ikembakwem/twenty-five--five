93e7b669cad033680fe9a0ea67d08244b7aa0a58
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-08-18 08:30:27 +0100

    Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { ComponentProps } from "react";
+
+interface ButtonProps extends ComponentProps<"button"> {
+  label: string;
+  size?: "sm" | "md";
+}
+
+export const Button = ({ label, size = "sm", ...props }: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={`flex items-center justify-center h-11 ${
+        size === "md" ? "" : "w-11"
+      } p-4 bg-gray-900 text-white leading-4 font-semibold rounded-full`}
+    >
+      {label}
+    </button>
+  );
+};
