3be73ed021c38e0cd50ce963ef6dce13df3b7288
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-08-19 14:13:48 +0100

    Beep sound after every countdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Break } from "./components/Break";
 import { Session } from "./components/Session";
 import { Timer } from "./components/Timer";
@@ -14,13 +14,11 @@ function App() {
       interval = setInterval(() => {
         if (timer > 0) {
           dispatch({ type: "countdown" });
-          console.log("Running countdown");
         }
         if (timer === 0) {
           mode === "session"
             ? dispatch({ type: "switchToBreakMode" })
             : dispatch({ type: "switchToSessionMode" });
-          console.log("Switching mode from", mode);
         }
       }, 1000);
     }
@@ -31,12 +29,25 @@ function App() {
 
   return (
     <div>
-      <div className="flex flex-col items-center gap-6 max-w-xl mx-auto mt-16 p-12 border-2 border-gray-600 rounded-2xl">
+      <h1 className="text-5xl text-center text-gray-800 mt-8 font-bold leading-tight tracking-tight">
+        25 + 5 Clock
+      </h1>
+      <div className="flex flex-col items-center gap-6 max-w-xl mx-auto mt-6 p-6 border-2 border-gray-600 rounded-2xl">
         <div className="flex justify-between self-stretch py-3">
           <Break />
           <Session />
         </div>
         <Timer />
+      </div>
+      <div className="flex justify-center items-baseline py-4 text-gray-600 text-lg">
+        <p>Designed and coded by</p>
+        <a
+          target="_blank"
+          href="https://ikembakwem.vercel.app/"
+          className="font-semibold pl-2 text-blue-800"
+        >
+          Ikechukwu Mbakwem
+        </a>
       </div>
     </div>
   );
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,12 +1,28 @@
 import { Button } from "./Button";
 import { useClock } from "../contexts/ClockConext";
+import { useEffect, useRef } from "react";
 
 export const Timer = () => {
   const { timer, isRunning, dispatch, mode } = useClock();
-  const seconds = Math.floor(timer % 60);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
+  useEffect(() => {
+    if (timer === 0 && audioRef.current) {
+      audioRef.current.play();
+    }
+  }, [timer]);
+
+  const seconds = Math.floor(timer % 60);
   const formattedMin = String(Math.floor(timer / 60)).padStart(2, "0");
   const formattedSec = String(seconds).padStart(2, "0");
+
+  const handleReset = () => {
+    dispatch({ type: "reset" });
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+    }
+  };
   return (
     <div className="flex flex-col items-center border-2 border-zinc-400 rounded-2xl py-6 px-8">
       <div
@@ -27,13 +43,14 @@ export const Timer = () => {
           size="md"
           onClick={() => dispatch({ type: "playPause" })}
         />
-        <Button
-          id="reset"
-          label="Reset"
-          size="md"
-          onClick={() => dispatch({ type: "reset" })}
-        />
+        <Button id="reset" label="Reset" size="md" onClick={handleReset} />
       </div>
+      <audio
+        ref={audioRef}
+        id="beep"
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        preload="auto"
+      ></audio>
     </div>
   );
 };
